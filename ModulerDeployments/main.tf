5a05dcbf92fd84fd362650523c94b05558cba4f2
Pankaj Agarwal <2005pank@gmail.com>
2023-05-21 17:26:53 +0530

    Terraform graph - Implicit dependencies null_resource.dockervol.id added in join function of container to create implicit dependency between nodered-container and dockervol

--- ModulerDeployments/main.tf
+++ ModulerDeployments/main.tf
@@ -5,7 +5,7 @@ module "image" {
 
 resource "null_resource" "dockervol" {
   provisioner "local-exec" {
-    command = "mkdir noderedvol/ ||true && sudo chown -R 1000:1000 noderedvol/"
+    command = "sleep 60 && mkdir noderedvol/ ||true && sudo chown -R 1000:1000 noderedvol/"
   }
 }
 resource "random_string" "random" {
@@ -18,7 +18,7 @@ resource "random_string" "random" {
 
 resource "docker_container" "nodered_container" {
   count = local.container_count
-  name  = join("-",["nodered",terraform.workspace,random_string.random[count.index].result])
+  name  = join("-",["nodered",terraform.workspace,null_resource.dockervol.id,random_string.random[count.index].result])
   image = module.image.image_out
   ports {
     internal = var.int_port
