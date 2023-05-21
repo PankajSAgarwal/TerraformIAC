e14228122275f4c5d17ee33627e5dcddb5aaadde
Pankaj Agarwal <2005pank@gmail.com>
2023-05-21 22:06:48 +0530

    container module

--- TerraformBasicsAndDocker/outputs.tf
+++ TerraformBasicsAndDocker/outputs.tf
@@ -1,9 +1,9 @@
-output "container_name" {
-  value = docker_container.nodered_container[*].name
-  description = "The name of the container"
-}
-
-output "ip-address" {
-  value = [for i in docker_container.nodered_container[*]:join(":",[i.network_data[0].ip_address],i.ports[*]["external"])]
-  description = "The ip address and external port of the container"
-}
+#output "container_name" {
+#  value = docker_container.nodered_container[*].name
+#  description = "The name of the container"
+#}
+#
+#output "ip-address" {
+#  value = [for i in docker_container.nodered_container[*]:join(":",[i.network_data[0].ip_address],i.ports[*]["external"])]
+#  description = "The ip address and external port of the container"
+#}
