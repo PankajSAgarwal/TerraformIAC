7cabb8097db5435957d31d3aa6ab4a8342f79e79
Pankaj Agarwal <2005pank@gmail.com>
2023-05-28 08:52:59 +0530

    VPC subnets "Hardcoded"

--- terraform-aws/networking/main.tf
+++ terraform-aws/networking/main.tf
@@ -12,3 +12,14 @@ resource "aws_vpc" "pankaj_vpc" {
     Name = "pankaj-vpc-${random_integer.random.id}"
   }
 }
+
+resource "aws_subnet" "pankaj_public_subnet" {
+  vpc_id = aws_vpc.pankaj_vpc.id
+  count = length(var.public_cidrs)
+  cidr_block = var.public_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone = ["us-west-2a","us-west-2b","us-west-2c","us-west-2d"][count.index]
+  tags = {
+    Name = "pankaj_public_${count.index + 1}"
+  }
+}
