aaa8ec4de25d7c2223ffe9427b6cbcc333a2711c
Pankaj Agarwal <2005pank@gmail.com>
2023-05-28 19:33:18 +0530

    Basic RDS Setup VPC Outputs for RDS Managing sensitive RDS Values

--- terraform-aws/networking/outputs.tf
+++ terraform-aws/networking/outputs.tf
@@ -2,3 +2,9 @@
 output "vpc_id" {
   value = aws_vpc.pankaj_vpc.id
 }
+output "db_subnet_group_name" {
+  value = aws_db_subnet_group.pankaj_rds_subnetgroup.*.name
+}
+output "db_security_group" {
+  value = [aws_security_group.pankaj_sg["rds"].id]
+}
