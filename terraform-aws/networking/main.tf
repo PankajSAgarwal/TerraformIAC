ceb397a3cd4046041ef7ac919eda608a208eddf4
Pankaj Agarwal <2005pank@gmail.com>
2023-05-28 09:52:00 +0530

    using resource random_shuffle_resource to randomly assign AZs.

--- terraform-aws/networking/main.tf
+++ terraform-aws/networking/main.tf
@@ -4,7 +4,10 @@ resource "random_integer" "random" {
   min = 1
   max = 100
 }
-
+resource "random_shuffle" "az_list" {
+  input = data.aws_availability_zones.available.names
+  result_count = var.max_subnets
+}
 resource "aws_vpc" "pankaj_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
@@ -19,7 +22,7 @@ resource "aws_subnet" "pankaj_public_subnet" {
   count                   = var.public_sn_count
   cidr_block              = var.public_cidrs[count.index]
   map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  availability_zone       = random_shuffle.az_list.result[count.index]
   tags = {
     Name = "pankaj_public_${count.index + 1}"
   }
@@ -30,7 +33,7 @@ resource "aws_subnet" "pankaj_private_subnet" {
   count                   = var.private_sn_count
   cidr_block              = var.private_cidrs[count.index]
   map_public_ip_on_launch = false
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  availability_zone       = random_shuffle.az_list.result[count.index]
   tags = {
     Name = "pankaj_private_${count.index + 1}"
   }
